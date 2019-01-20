9c24882718e7a6280d8871654b3a8a4f0d1f2927
Javier Fernando Rojas Chavez <jnando.rc@gmail.com>
2019-01-20 15:02:10 -0500

    first commit
    send source

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require_once('lib/ThreeScaleClient.php');
+
+// initialize
+$client = new ThreeScaleClient();
+
+// the service token
+$service_token = "7817e8cd5df81d0429941ae8b4c0b717761d18df9261904046ee3fb727379684";
+
+// the service_id
+$service_id = "2555417766154";
+
+// you will usually obtain app_id and app_key from the request params
+$user_key = "9d364e3cef20438858970155df5afce2";
+
+$response = $client->authrep_with_user_key($user_key, new ThreeScaleClientCredentials($service_id, $service_token), array('hits' => 1));
+
+if ($response->isSuccess()) {
+  // All fine, proceeed.
+	echo "fine!";
+} else {
+  // Something's wrong with this app.
+	echo "worng";
+}		  
+?>
